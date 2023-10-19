d816a7b1393943e4156f70115f0056c5e3c543be
royarkaprava <arkapravaroy0075@gmail.com>
2023-10-19 14:36:12 -0400

    Update Conjugate gradient.R

--- miscellaneous/Conjugate gradient.R
+++ miscellaneous/Conjugate gradient.R
@@ -7,26 +7,27 @@ LinearCG <- function(A, b, x0, tol=1e-5){
   n <- nrow(A)
   
   num_iter = 0
-  
+  alphadkvec <- 0
   while(error > tol){
     adk = A %*% dk
     rk2old = sum(rk^2)
     
     alpha = rk2old / sum(dk*adk)
+    
     xk = xk + alpha * dk
     rk = rk - alpha * adk
     beta = sum(rk^2) / rk2old
     dk = rk + beta * dk
     
     num_iter = num_iter + 1
-    
+    alphadkvec[num_iter] <- alpha*sqrt(sum(dk^2))
     if(num_iter > n){break}
     error <- sum(rk^2)
   }
   
   rk_norm = sqrt(sum(rk^2))
   
-  return(xk)
+  return(list(sol=xk, alpha=alphadkvec))
 }
 Rcpp::sourceCpp("Conjugate_gradient.cpp")
 
@@ -43,7 +44,8 @@ system.time(x3 <- LinearCGc(A, b, rep(0, p), tol=1e-4))
 mean((x3-beta)^2)
 
 system.time(x2 <- LinearCG(A, b, rep(0, p), tol=1e-4))
-mean((x2-beta)^2)
+plot(x2$alpha)
+mean((x2$sol-beta)^2)
 
 system.time(x1 <- lm(y~X-1)$coefficients)
 mean((x1-beta)^2)
