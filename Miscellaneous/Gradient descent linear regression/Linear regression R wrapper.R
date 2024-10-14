df978d4d8ac20d324fc951ea371ad929b678e7ec
royarkaprava <34636927+royarkaprava@users.noreply.github.com>
2024-10-14 13:53:39 -0400

    Update Linear regression R wrapper.R

--- Miscellaneous/Gradient descent linear regression/Linear regression R wrapper.R
+++ Miscellaneous/Gradient descent linear regression/Linear regression R wrapper.R
@@ -5,7 +5,7 @@ y = 3+2*x + rnorm(1000, 0, 1)
 
 X <- cbind(rep(1, 1000), x)
 
-gradientdescentfitR(X, y, 1000, 0.01, 0)
+gradientdescentfitR(X, y, 1000, 1, 0)
 
 ##Using the Rcpp code
 betaGD = gradientdescentfit(X, y, 1000, 0.01, 0)
